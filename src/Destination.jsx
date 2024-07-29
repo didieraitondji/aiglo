9fcce3d780c8952aad0f63dbf41eb75d98f02468
didos <didier.tolomeaitondji@gmail.com>
2024-07-29 19:36:01 +0100

    v2 raccine

---
+++ src/Destination.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Destination() {
+    return (
+        <>
+            <div className=''>
+
+            </div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Destination from './Destination';
 
 
+const Root = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Destination />} />
+      </Routes>
+    </Router>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<Root />);
